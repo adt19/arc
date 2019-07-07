7d81c74e88e3e9ed8447376b5076b8f05381c3d9
YW1wb3MK <nelson211145+ampos@gmail.com>
2019-07-07 22:32:47 +0800

    fix(news): backend image path

--- src/news/backend.ts
+++ src/news/backend.ts
@@ -1,6 +1,7 @@
 import { Item } from './model/item';
 import { of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { environment as env } from 'src/environments/environment';
 
 /* tslint:disable-next-line:max-line-length */
 const sample = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
@@ -11,7 +12,7 @@ function gen(length, p) {
     return new Item({
       title: 'Item - ' + pi,
       desc: sample.substr(0, sample.length * (pi % 9 + 1) / 10),
-      img: '/assets/img/brand_12' + (pi % 4 === 0 ? '' : '8') + '.png',
+      img: env.base + '/assets/img/brand_12' + (pi % 4 === 0 ? '' : '8') + '.png',
       updatedAt: (Date.now() - pi * 12345_000) / 1000
     });
   });
